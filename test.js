9828e59ed4ce1432b34ee04e2a79e9da0b5114f8
Daniel Allen <danielpeterallen@gmail.com>
2023-08-01 12:49:25 +1000

    Request for task change done

---
+++ test.js
@@ -0,0 +1,66 @@
+
+function replaceFunction() {
+    let text = document.getElementById("example").innerHTML; 
+    document.getElementById("example").innerHTML =
+      text.replace("pink", "PURPLE");
+  }
+
+function locateFunction() {
+    let text2 = "This is the string - Please locate where the last occurance of the string occurs!";
+    let index = text2.lastIndexOf("the string");
+    document.getElementById("demo").innerHTML = index;
+  }
+  
+  function helloWorld() {
+    let danTest = "Hello, World!";
+    let index = danTest.indexOf("W");
+    document.getElementById("demo2").innerHTML = index;
+  }
+
+  function splitArray() {
+    let danString2 = "Hello World";
+    let splitArray = danString2.split(", ");
+    document.getElementById("demo3").innerHTML = splitArray;
+  }
+
+  function changeCase() {
+    let danString3 = "Oh what a wonderful world we are living in!";
+    let upperCaseString = danString3.toUpperCase();
+    let lowerCaseString = danString3.toLowerCase();
+    
+    document.getElementById("demo4").innerHTML = upperCaseString;
+    
+  }
+
+  function locateFunction() {
+    let text2 = "This is the string - Please locate where the last occurance of the string occurs!";
+    let index = text2.lastIndexOf("the string");
+    document.getElementById("demo").innerHTML = index;
+  }
+
+  function locateFunction() {
+    let text2 = "This is the string - Please locate where the last occurance of the string occurs!";
+    let index = text2.lastIndexOf("the string");
+    document.getElementById("demo").innerHTML = index;
+  }
+
+  function locateFunction() {
+    let text2 = "This is the string - Please locate where the last occurance of the string occurs!";
+    let index = text2.lastIndexOf("the string");
+    document.getElementById("demo").innerHTML = index;
+  }
+
+
+// let firstName = "Peter";
+// let lastName = "Blogs";
+// let fullName = firstName.concat(" ", lastName);
+
+// console.log(fullName);
+
+// let danString4 = "This is the house that Jack built!";
+// let startsWith = danString4.startsWith("This");
+// let endsWith = danString4.endsWith("BOOMARANG!");
+
+// console.log(startsWith);
+// console.log(endsWith);
+
